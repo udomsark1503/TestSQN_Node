cc598b8b07646eaf10627beb832682fc42b72ebf
Udomsark Amnuaysookwong <36187662+udomsark1503@users.noreply.github.com>
2023-11-07 11:43:34 +0700

    Update ReadDataFromPG.js

--- controller/ReadDataFromPG.js
+++ controller/ReadDataFromPG.js
@@ -6,13 +6,11 @@ router.use(express.json());
 
 router.get("/PullData", async (req, res) => {
   try {
-    const year = req.query.year || '1950';
     const result = await pool.query(`
       SELECT *
       FROM populationdata
       WHERE "Year" = '1950'
-      ORDER BY "Country name"
-    `, [year]);
+      ORDER BY "Country name"`);
     res.json(result.rows);
   } catch (error) {
     res.status(500).send("Internal Server Error");
